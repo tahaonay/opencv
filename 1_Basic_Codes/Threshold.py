97e1a85f49804916b31cbb1a21c659fd0efab22f
Taha <onaytaha@gmail.com>
2023-03-21 19:49:02 +0300

    Update and rename esik.py to 1_Basic_Codes/Threshold.py

--- 1_Basic_Codes/Threshold.py
+++ 1_Basic_Codes/Threshold.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 img = cv2.imread("img1.jpg")
 img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 plt.figure()
-plt.imshow(img,cmap ="gray")  # resmi gri tonlamalı yapar
+plt.imshow(img,cmap ="gray")  # black-white image
 plt.axis("off")
 plt.show()
 
 
 
-# eşikleme beyaz
+# thresh white
 _,thresh_img = cv2.threshold(img,thresh =60,maxval=255,type= cv2.THRESH_BINARY)
 plt.figure()
 plt.imshow(thresh_img, cmap="gray")
 plt.axis("off")
 plt.show()
 
-# esikleme siyah
+# thresh black
 _,thresh_img2 = cv2.threshold(img,thresh =60,maxval=255,type= cv2.THRESH_BINARY_INV)
 plt.figure()
 plt.imshow(thresh_img2, cmap="gray")
@@ -25,7 +25,7 @@ plt.axis("off")
 plt.show()
 
 
-# uyarlamalı eşik değeri
+# adjustable thresh
 
 thresh_img2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,8)
 plt.figure()
